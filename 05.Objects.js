1c4ac0856876634c67ac803d2fb466af1369b637
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-01-16 15:50:06 +0545

    Objects re-studied

--- 05.Objects.js
+++ 05.Objects.js
@@ -1,4 +1,4 @@
-var user = {
+var Prabin = {
     First_name : "Prabin",
     Last_name : "Baniya",
     Country : "Nepal",
@@ -7,7 +7,20 @@ var user = {
     }
 }
 
-console.log(user.First_name);
-console.log(user["Last_name"]);
-console.log(user.get_full_name());
+console.log(Prabin.First_name);
+console.log(Prabin["Last_name"]);
+console.log(Prabin.get_full_name());
+
+var Student = function(first_name, last_name, country){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.country = country;
+}
+
+var john = new Student("John","Albert","USA");  // Creates a new Objects
+var sam = new Student("Sam","Chrits","Russia"); // Creates a new Objects
+
+console.log(john.first_name);
+console.log(sam.last_name);
+console.log(john.country);
 
