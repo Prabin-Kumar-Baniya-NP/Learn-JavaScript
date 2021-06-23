61457e03a27c41b268ae621520e85d885da7c7b3
Prabin-Kumar-Baniya-NP <pra44binind@gmail.com>
2021-06-23 15:30:15 +0545

    Understanding Fetch API in Javascript

---
+++ 28.FetchAPI.js
@@ -0,0 +1,16 @@
+url = 'https://reqres.in/api/users/23';
+params = {
+    method: 'POST',
+    headers: {
+        'Content-type' : 'application/json',
+    },
+    body: JSON.stringify({
+        name: 'User 1',
+    })
+}
+
+
+fetch(url, params).
+    then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.log('Error'))
